31f9af0c753ceb588ee37170efda1632a703e530
nicolasgutierrez <nicolasgutierrez@192.168.1.105>
2018-08-15 21:24:45 -0300

    Capa logica Controaldores Fabrica e Interfaces

---
+++ src/Logica/culturarteFabrica.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author nicolasgutierrez
+ */
+public class culturarteFabrica {
+    
+    private static culturarteFabrica instance;
+    
+    public static culturarteFabrica getInstance() {
+        if(instance==null){
+            instance= new culturarteFabrica();
+        }
+            return instance;
+    }
+    
+        private culturarteFabrica(){
+            
+        }
+        
+        public void cargarDatosPrueba(){
+            iConColaboracion ic= this.getIContColaboracion();
+            iConUsuario iu= this.getIContUsuario();
+            iConPropuesta ip= this.getIContPropuesta();
+        }
+    
+        public iConColaboracion getIContColaboracion(){
+            iConColaboracion icc= ContColaboracion.getInstance();
+            return icc;
+        }
+        
+        public iConUsuario getIContUsuario(){
+            iConUsuario icu= ContUsuario.getInstance();
+            return icu;
+        }
+        
+        public iConPropuesta getIContPropuesta(){
+            iConPropuesta icp= ContPropuesta.getInstance();
+            return icp;
+        }
+    
+}
