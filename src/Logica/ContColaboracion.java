425f16adf8519f8035f7335a83fba0e7d80c672a
JpBallejo <jballejo@gmail.com>
2018-09-06 09:06:17 -0300

    carga y limpiar

--- src/Logica/ContColaboracion.java
+++ src/Logica/ContColaboracion.java
@@ -20,7 +20,7 @@ public class ContColaboracion implements iConColaboracion {
     private colProp colaboracion;
     private ContUsuario cUsuario = ContUsuario.getInstance();
     private colaboracionesPersistencia colPer = new colaboracionesPersistencia();
-    ContCargaBD contCarga = ContCargaBD.getInstance();
+    private ContCargaBD contCarga = ContCargaBD.getInstance();
     public static ContColaboracion getInstance() {
         if (instance == null) {
             instance = new ContColaboracion();
